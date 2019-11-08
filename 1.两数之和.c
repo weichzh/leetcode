e4e88dc8e07ab8a655542a405e39aa57659a6176
nick <shx123qw@163.com>
2019-11-08 22:09:32 +0800

    修改两数之和.c的暴力法代码

--- 1.两数之和.c
+++ 1.两数之和.c
@@ -20,13 +20,13 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
         for (int j = i + 1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
                 *returnSize = 2;
-                nums[0] = i;
-                nums[1] = j;
-                return nums;
+                result[0] = i;
+                result[1] = j;
+                return result;
             }
         }
     }
-    return nums; 
+    return result; 
 }
 */
 
